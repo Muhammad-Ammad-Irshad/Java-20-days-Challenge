5326cac3e6cede6aa289b478a9e876976bdc6f3b
Muhammad Ammad Irshad <ammad9997a@gmail.com>
2025-07-28 19:08:53 +0500

    Add files via upload

---
+++ Day_10/Shape.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.day_10;
+
+/**
+ *
+ * @author DELL 5410
+ */
+public abstract class Shape {
+    public abstract double area();
+    public abstract String getName();
+}
