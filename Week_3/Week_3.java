bac390164bfd16a9dd55a867603e1dcc23670908
Muhammad Ammad Irshad <ammad9997a@gmail.com>
2025-08-02 18:32:24 +0500

    Add files via upload

---
+++ Week_3/Week_3.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.week_3;
+
+/**
+ *
+ * @author DELL 5410
+ */
+public class Week_3 {
+
+    public static void main(String[] args) {
+        Patient p1 = new Patient("Pogba", "P10", 32, "ACL Injury");
+        p1.displayInfo();
+    }
+}
