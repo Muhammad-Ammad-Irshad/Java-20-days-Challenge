908a4d9ec493a470251e8a1d0d6dffbde2a22dd3
Muhammad Ammad Irshad <ammad9997a@gmail.com>
2025-08-02 18:32:03 +0500

    Add files via upload

--- Week_3/Patient.java
+++ Week_3/Patient.java
@@ -1,1 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.week_3;
 
+/**
+ *
+ * @author DELL 5410
+ */
+public class Patient {
+    private String name;
+    private String id;
+    private int age;
+    private String medicalHistory;
+    
+    public Patient(String name, String id, int age, String medicalHistory){
+        this.name = name;
+        this.id = id;
+        this.age = age;
+        this.medicalHistory = medicalHistory;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    public String getId(){
+        return id;
+    }
+    public int getAge(){
+        return age;
+    }
+    public String getMedicalHistory(){
+        return medicalHistory;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setId(String id){
+        this.id = id;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public void setMedicalHistory(String medicalHistory){
+        this.medicalHistory = medicalHistory;
+    }
+    
+    public void displayInfo(){
+        System.out.println("Patient ID: "+id);
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+        System.out.println("History: "+medicalHistory);
+        
+    }
+}
