166413e2716441bdf2f13577b3950708337a0a90
Muhammad Ammad Irshad <ammad9997a@gmail.com>
2025-08-05 15:11:14 +0500

    Add files via upload

--- Week_3/Week_3.java
+++ Week_3/Week_3.java
@@ -13,11 +13,13 @@ public class Week_3 {
     public static void main(String[] args) {
         Patient p1 = new Patient("Michal", "P10", 26, "Diabetes");
         p1.displayInfo();
+        p1.saveToFile();
         
         Doctor d1 = new Doctor("Dr. Sara", "S123", "Cardiologist", true);
         d1.displayInfo();
         
         Appointment a1 = new Appointment(p1, d1, "2025-08-04", "10:30 AM");
         a1.displayInfo();
+        a1.saveToFile();
     }
 }
