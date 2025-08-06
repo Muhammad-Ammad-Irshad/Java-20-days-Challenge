09b5100dbf72ba0bed59e28385993db4060a3870
Muhammad Ammad Irshad <ammad9997a@gmail.com>
2025-08-06 14:33:34 +0500

    Add files via upload

--- Week_3/Week_3.java
+++ Week_3/Week_3.java
@@ -3,7 +3,7 @@
  */
 
 package com.mycompany.week_3;
-
+import java.util.Scanner;
 /**
  *
  * @author DELL 5410
@@ -11,15 +11,41 @@ package com.mycompany.week_3;
 public class Week_3 {
 
     public static void main(String[] args) {
-        Patient p1 = new Patient("Michal", "P10", 26, "Diabetes");
-        p1.displayInfo();
+        Scanner scanner = new Scanner(System.in);
+        
+        Patient p1 = new Patient("Michal", "M123", 26, "Diabetes");
         p1.saveToFile();
         
         Doctor d1 = new Doctor("Dr. Sara", "S123", "Cardiologist", true);
-        d1.displayInfo();
         
         Appointment a1 = new Appointment(p1, d1, "2025-08-04", "10:30 AM");
-        a1.displayInfo();
         a1.saveToFile();
+        
+        while(true){
+            System.out.println("\n---Hospital Management System---");
+            System.out.println("1. Search Patient");
+            System.out.println("2. Delete Patient");
+            System.out.println("3. Exit");
+            System.out.print("Choose option: ");
+            int choose = scanner.nextInt();
+            
+            switch(choose){
+                case 1: 
+                    System.out.print("Enter Patient ID to search: ");
+                    String searchId = scanner.nextLine();
+                    HospitalUtils.searchPatientById(searchId);
+                    break;
+                case 2:
+                    System.out.print("Enter Patient ID to delete: ");
+                    String deleteId = scanner.nextLine();
+                    HospitalUtils.deletePatientById(deleteId);
+                    break;
+                case 3:
+                    System.out.println("Exiting..");
+                    return;
+                default:
+                    System.out.println("Invalid option");
+            }
+        }
     }
 }
